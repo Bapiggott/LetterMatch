09bea8d7c81d9f2cf397c89d96217e187ac067f5
Brett Piggott <bapiggott@oakland.edu>
2025-02-15 14:59:35 -0500

    Added database migrations

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,33 @@
 from flask import Flask, jsonify
+from datetime import datetime, timedelta
+from flask import Flask, request, jsonify
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
+import traceback
+from flask_cors import CORS
+import jwt
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///LetterMatch.db'
+app.config['SECRET_KEY'] = str('VerySecretKey!')
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+db = SQLAlchemy(app)
+# CORS(app)
+migrate = Migrate(app, db)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    role = db.Column(db.Integer, default=1)  # Regular user = role 1
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route("/")
 def home():
     return jsonify({"message": "Backend is running!"})
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True, port=5000)
