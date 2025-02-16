f0cea2c174f48b51c5f16a5d810c129bcbb83607
ecarnovsky <ecarnovsky@gmail.com>
2025-02-15 22:38:50 -0500

    Fixed header selectors in css file

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,12 +1,15 @@
 // HomePage.jsx
 import React from 'react';
+import Layout from '../Layout/Layout';
 
 const HomePage = () => {
     return (
-        <div>
-            <h2>HomePage</h2>
-            {/* Your form code goes here */}
-        </div>
+        <Layout>
+            <div>
+                <h2>HomePage</h2>
+                {/* Your form code goes here */}
+            </div>
+        </Layout>
     );
 };
 
