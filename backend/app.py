9db93482f3b87e019273b4fcecb5e6efffe7b4a2
Brett Piggott <bapiggott@oakland.edu>
2025-02-20 12:49:19 -0500

    User backend

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import traceback
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_sqlalchemy import SQLAlchemy
@@ -125,7 +126,67 @@ def submit_word(data):
         else:
             emit('word_rejected', {"message": "Word does not follow the chain rule"}, room=room)
 
+    
+def get_jwt_token(request):
+    token = request.headers.get('Authorization')
+    if not token:
+        return jsonify({"message": "Token is missing!"}), 401
+    try:
+        token = token.split(" ")[1] 
+        decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        user_id = decoded_token['user_id']
+        return user_id
+    except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+        return jsonify({"message": "Invalid or expired token!"}), 401
 
+@app.route("/register", methods=["POST"])
+def register():
+    try: 
+        data = request.get_json()
+        if not data:
+            return jsonify({"message": "No input data provided"}), 400
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+        #role = data.get('role')
+        if not username or not email or not password:
+            return jsonify({"message": "Missing input data"}), 400
+        if User.query.filter_by(username=username).first():
+            return jsonify({"message": "Username already exists"}), 400
+        if User.query.filter_by(email=email).first():
+            return jsonify({"message": "Email already exists"}), 400
+        hashed_password = generate_password_hash(password, method='sha256')
+        newUser = User(username=username, email=email, password=hashed_password, role=1)
+        db.session.add(newUser)
+        db.session.commit()
+        return jsonify({"message": "User successfully registered"}), 201
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        print(traceback.format_exc())
+        return jsonify({"message": f"An error occurred: {str(e)}"}), 400
+
+@app.route("/login", methods=["POST"])
+def login():
+    try:
+        data = request.get_json()
+        if not data:
+            return jsonify({"message": "No input data provided"}), 400
+        email = data.get('email')
+        password = data.get('password')
+        if not email or not password:
+            return jsonify({"message": "Missing input data"}), 400
+        user = db.session.query(User.id, User.email, User.role, User.date_created).filter_by(email=email).first()
+        if not user:
+            return jsonify({"message": "User does not exist"}), 400
+        if not check_password_hash(user.password, password):
+            return jsonify({"message": "Invalid password"}), 400
+        token = jwt.encode({'user_id': user.id, 'exp': datetime.utcnow() + timedelta(hours=24)}, app.config['SECRET_KEY'], algorithm='HS256')
+        return jsonify({"message": "Login Successful", "token": token, "user": {"id": user.id, "email": user.email, "role": user.role, "date_created": user.date_created}}), 200
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        print(traceback.format_exc())
+        return jsonify({"message": f"An error occurred: {str(e)}"}), 400
+    
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Ensure database tables exist
