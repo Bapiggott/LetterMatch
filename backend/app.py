320ae804e485c48d9afe49ce1b757e270e120db4
ecarnovsky <ecarnovsky@gmail.com>
2025-02-19 22:10:18 -0500

    Added add-friend endpoint

--- backend/app.py
+++ backend/app.py
@@ -24,18 +24,52 @@ class User(db.Model):
     role = db.Column(db.Integer, default=1)  # Regular user = role 1
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+    friendships_1 = db.relationship('Friendship', foreign_keys='Friendship.username_1', backref='user_1', lazy=True)
+    friendships_2 = db.relationship('Friendship', foreign_keys='Friendship.username_2', backref='user_2', lazy=True)
+
+
 
 class Friendship(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username_1 = db.Column(db.String, foreign_key=True, nullable=False)
-    username_2 = db.Column(db.String, foreign_key=True, nullable=False)
+    username_1 = db.Column(db.String, db.ForeignKey('user.username'), nullable=False)
+    username_2 = db.Column(db.String, db.ForeignKey('user.username'), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route("/")
 def home():
     return jsonify({"message": "Backend is running!"})
 
+
+def addUser(username, email, password, role):
+        new_user = User(username=username, email=email, password=password, role=role )
+        db.session.add(new_user)
+        db.session.commit()
+
+# The below function will only work if both users have already 
+# been added to the database
+@app.route("/add-friend", methods=["POST"])
+def add_friend():
+    try:
+
+        data = request.get_json() 
+
+        username = data.get('username')
+        addFriendUsername = data.get('addFriendUsername')
+
+        print(f"About to add friendship between {addFriendUsername} and {username}.")
+
+        friendship = Friendship(username_1=username, username_2=addFriendUsername)
+        db.session.add(friendship)
+        db.session.commit()
+
+        return jsonify({"message": "Friend successfully added"}), 200
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": "Failed to add friend"}), 400
+
 if __name__ == "__main__":
     with app.app_context():  # Ensure app context is set
         db.create_all()  # Create tables
     app.run(debug=True, port=5000)
+
