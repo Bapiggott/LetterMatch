85baddeea7403e4926b0ebaec33eec0bc0ac46cf
ecarnovsky <ecarnovsky@gmail.com>
2025-02-19 22:30:43 -0500

    Added code to friends.jsx and added config file

--- frontend/src/components/FriendsPage/Friends.jsx
+++ frontend/src/components/FriendsPage/Friends.jsx
@@ -2,13 +2,52 @@
 import React from 'react';
 import Layout from '../Layout/Layout';
 import './Friends.css'
+import { API_URL } from '../../config';
+import { useState } from 'react';
 
 const Friends = () => {
+
+    const [addFriendUsername, setAddFriendUsername] = useState("")
+
+    const updateAddFriendUsername = (event) => {
+        setAddFriendUsername(event.target.value);
+    }
+
+    const addFriend = async () => {
+        try {
+
+            event.preventDefault()
+
+            const response = await fetch(`${API_URL}/add-friend`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                        username: "user234",
+                        addFriendUsername: addFriendUsername
+                    })
+            })
+
+            if (!response.ok) {
+                throw new Error("API response not okay.")
+            }
+
+            const data = await response.json()
+            console.log(data)
+        } catch (error) {
+            console.error("Error:", error)
+        }
+    }
+
+
     return (
         <Layout>
             <div>
                 <h2>Friends</h2>
-                {/* Your form code goes here */}
+                <form onSubmit={addFriend}  method='POST'>
+                    <label htmlFor="add-friend-name">Friend to add: </label>
+                    <input onChange={updateAddFriendUsername} id='add-friend-name' type="text" name="addFriendUsername"/>
+                    <button type='submit'>Add Friend</button>
+                </form>
             </div>
         </Layout>
     );
