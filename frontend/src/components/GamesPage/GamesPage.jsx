bd471243d46f5fe78b59334b86f5f5b0db3bb6f6
Christina2426 <cmarogi@oakland.edu>
2025-04-03 02:24:48 -0400

    Sdded more sounds to the Game Page

--- frontend/src/components/GamesPage/GamesPage.jsx
+++ frontend/src/components/GamesPage/GamesPage.jsx
@@ -5,12 +5,27 @@ import './GamesPage.css';
 const GamesPage = () => {
     // Reference audio file correctly from the public folder
     const cardFlipAudio = new Audio('/sounds/GameCard_Flip.mp3');
+    const playButtonAudio = new Audio('/sounds/PlayButton.wav');
 
-
-    // Function to play the sound when mouse enters the card
+    // Function to play the sound when mouse enters the card or when sound triggered
     const playCardFlipSound = () => {
-         console.log('Mouse Enter Triggered');
-        cardFlipAudio.play(); // Play sound on mouse enter
+        if (!cardFlipAudio.paused) {
+            cardFlipAudio.pause(); // Pause sound
+            cardFlipAudio.currentTime = 0;  // Reset sound
+        } else {
+            cardFlipAudio.play(); // Play sound
+        }
+        console.log('Mouse Enter Triggered - Card Flip Sound');
+    };
+
+    const playButtonSound = () => {
+        if (!playButtonAudio.paused) {
+            playButtonAudio.pause(); // Pauses sound
+            playButtonAudio.currentTime = 0;  // Resets sound
+        } else {
+            playButtonAudio.play(); // Play sound 
+        }
+        console.log('Mouse Enter Triggered - Play Button Sound');
     };
 
     return (
@@ -23,7 +38,7 @@ const GamesPage = () => {
                 </section>
                 <div className="all-games-div">
                     {/* Game 1 - Letter Match */}
-                    <div className="flip-card" onMouseEnter={playCardFlipSound}>
+                    <div className="flip-card" onMouseEnter={playCardFlipSound} onMouseLeave={playCardFlipSound}>
                         <div className="flip-card-inner">
                             <div className="flip-card-front">
                                 <div className="flip-card" style={{ backgroundImage: 'url("/images/letterMatch_coverImg.png")' }}></div>
@@ -42,14 +57,14 @@ const GamesPage = () => {
                                 <p>...ALL starting with the same letter? 🔠</p>
                                 <p className="small-text"> Both Single Player and Multiplayer Modes</p>
                                 <div className="btn-div">
-                                    <a className="header-style-btn play-btn" href="/lettermatch">🎮 Play</a>
+                                    <a className="header-style-btn play-btn"   href="/lettermatch"  onMouseEnter={playButtonSound}  > 🎮 Play</a>
                                 </div>
                             </div>
                         </div>
                     </div>
 
                     {/* Game 2 - Word Blitz */}
-                    <div className="flip-card" onMouseEnter={playCardFlipSound}>
+                    <div className="flip-card" onMouseEnter={playCardFlipSound} onMouseLeave={playCardFlipSound}>
                         <div className="flip-card-inner">
                             <div className="flip-card-front">
                                 <div className="flip-card" style={{ backgroundImage: 'url("/images/wordBlitz.png")' }}></div>
@@ -69,14 +84,14 @@ const GamesPage = () => {
                                 <p className="small-text">See how your friends would answer! ⏳</p>
                                 <p className="small-text">Multiplayer Only</p>
                                 <div className="btn-div">
-                                    <a className="header-style-btn" href="/wordblitz">🎮 Play</a>
+                                    <a className="header-style-btn" href="/wordblitz" onMouseEnter={playButtonSound}  >🎮 Play</a>
                                 </div>
                             </div>
                         </div>
                     </div>
 
                     {/* Game 3 - Word Chain */}
-                    <div className="flip-card" onMouseEnter={playCardFlipSound}>
+                    <div className="flip-card" onMouseEnter={playCardFlipSound} onMouseLeave={playCardFlipSound}>
                         <div className="flip-card-inner">
                             <div className="flip-card-front">
                                 <div className="flip-card" style={{ backgroundImage: 'url("/images/wordChain.png")' }}></div>
@@ -94,7 +109,7 @@ const GamesPage = () => {
                                 <p className="small-text">Be quick - Time is ticking or face ELIMINATION⏳</p>
                                 <p className="small-text">Multiplayer Only</p>
                                 <div className="btn-div">
-                                    <a className="header-style-btn" href="/wordchain">🎮 Play</a>
+                                    <a className="header-style-btn"  href="/wordchain" onMouseEnter={playButtonSound}  > 🎮 Play</a>
                                 </div>
                             </div>
                         </div>
